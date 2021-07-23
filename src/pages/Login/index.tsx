4f450b8fc64b2e0983424708bf2bd31ef2686f86
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-07-22 22:13:18 -0300

    feat: adjust layout and inset qrcode

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
 import './styles.css';
 
 function Login(): JSX.Element {
+  const [qrcode, setQrcode] = useState('');
+
+  const generateSecret = async () => {
+    try {
+      const response = await axios.get('http://localhost:3333/');
+      console.log(response.data.secret);
+
+      const response2 = await axios.post(
+        'http://localhost:3333/generate_qr_code',
+        {
+          secret: response.data.secret,
+        }
+      );
+
+      console.log(response2);
+
+      setQrcode(response2.data.qr_code);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="container">
-      <input type="text" placeholder="Usuário" />
-      <input type="text" placeholder="Senha" />
-      <button type="button">Fazer login</button>
+      <button onClick={generateSecret} type="button">
+        Gerar Segredo
+      </button>
+      {qrcode && (
+        <div className="login-info">
+          <img src={qrcode} alt="QrCode" />
+          <div className="verify-form">
+            <input type="text" />
+            <button type="button">Verificar</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
