feb9f23e6c3f64c5b95ac608e6a12797318d9ba9
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-07-22 23:19:25 -0300

    feat: deploy to heroku

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,22 +5,30 @@ import './styles.css';
 
 function Login(): JSX.Element {
   const [qrcode, setQrcode] = useState('');
+  const [secret, setSecret] = useState('');
+  const [token, setToken] = useState('');
+  const [validToken, setValidToken] = useState(false);
 
   const generateSecret = async () => {
     try {
-      const response = await api.get('/');
-      console.log(response.data.secret);
+      const response = await api.get('/get_secret');
+      setSecret(response.data.secret.ascii);
 
       const response2 = await api.post('/generate_qr_code', {
         secret: response.data.secret,
       });
-
-      console.log(response2);
-
       setQrcode(response2.data.qr_code);
     } catch (e) {
-      console.log(e);
+      alert('Erro ao gerar o QRCode');
     }
+  };
+
+  const verifyToken = async () => {
+    const response = await api.post('/verify_token', {
+      secret,
+      token,
+    });
+    setValidToken(response.data.token_is_valid);
   };
 
   return (
@@ -32,9 +40,12 @@ function Login(): JSX.Element {
         <div className="login-info">
           <img src={qrcode} alt="QrCode" />
           <div className="verify-form">
-            <input type="text" />
-            <button type="button">Verificar</button>
+            <input type="text" onChange={(e) => setToken(e.target.value)} />
+            <button type="button" onClick={verifyToken}>
+              Verificar
+            </button>
           </div>
+          {validToken && <p>O token é valido!</p>}
         </div>
       )}
     </div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://luiz-setten-challenge-backend.herokuapp.com',
 });
