6b1de1a6c52d646791fcc59581123d7f84dd94eb
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-07-22 23:31:30 -0300

    feat: deploy to heroku

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,6 +4,12 @@ function Home(): JSX.Element {
   return (
     <div className="container">
       <p>Olá usuário</p>
+      <a href="https://github.com/luizsetten/2fa-challenge-backend">
+        Repositório do backend
+      </a>
+      <a href="https://github.com/luizsetten/2fa-challenge-frontend">
+        Repositório do frontend
+      </a>
     </div>
   );
 }
