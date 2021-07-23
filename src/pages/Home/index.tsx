31afc29385e9e76a4076bfe67a2b4febefd8d98e
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-07-22 23:52:06 -0300

    feat: add description

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,9 +8,15 @@ function Home({ secret }: HomeProps): JSX.Element {
   return (
     <div className="container">
       <p>Olá usuário, sua secret key é: {String(secret)}</p>
+      <br />
+      <span>
+        No backend foi utilizado o ExpressJS, qrcode, speakeasy (para login em
+        duas eapas)
+      </span>
       <a href="https://github.com/luizsetten/2fa-challenge-backend">
         Repositório do backend
       </a>
+      <span>No frontend foi utilizado ReactJS e axios</span>
       <a href="https://github.com/luizsetten/2fa-challenge-frontend">
         Repositório do frontend
       </a>
